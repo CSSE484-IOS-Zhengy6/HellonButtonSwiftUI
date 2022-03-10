29cb1d0168bcd4a8d17bb4acdbe0572eb07f74db
travisz <zhengy6@rose-hulman.edu>
2022-03-10 09:20:03 -0500

    Initial Commit

---
+++ HellonButtonSwiftUI/HellonButtonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HellonButtonSwiftUIApp.swift
+//  HellonButtonSwiftUI
+//
+//  Created by Yuanhang on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HellonButtonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
